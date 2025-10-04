b1af44f8b7c38f6d3ba67fbd006d7b47d6f50283
ultimatewatagashi <ktc25a31g0015@edu.kyoto-tech.ac.jp>
2025-10-04 12:40:25 +0900

    feat: integrate Tailwind CSS and set up routing for Pokémon app
    - Added Tailwind CSS and its Vite plugin to the project.
    - Implemented routing using react-router-dom for Pokémon list and detail pages.
    - Created API utility functions to fetch Pokémon data and their Japanese names.
    - Defined TypeScript interfaces for Pokémon data structures.
    - Developed basic components for Pokémon list and detail views.
    - Configured global styles with Tailwind CSS and custom theme settings.

---
+++ src/api/common.type.ts
@@ -0,0 +1,14 @@
+export interface NamedAPIResource {
+    name: string;
+    url: string;
+}
+
+export interface Name {
+    name: string;
+    language : NamedAPIResource;
+}
+
+export interface APIResource {
+    url: string;
+}
+ 
